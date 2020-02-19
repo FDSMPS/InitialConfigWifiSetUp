03955896137c570a24d673c427aad4856861e4fc
Dorsa Nahid <31228267+dorsMaster@users.noreply.github.com>
2020-02-19 16:43:02 -0700

    Update readFile.py

--- readFile.py
+++ readFile.py
@@ -2,7 +2,7 @@ import os
 import json
 import subprocess
 
-f = open("creds.json", "r")
+f = open("../creds.json", "r")
 a = (f.read())
 f.close()
 
@@ -21,11 +21,10 @@ print(ssid)
 # if not os.path.exists(filePathToWrite):
 #     os.makedirs(filePathToWrite)
 
-toWriteToConfigFile = "network={\n\tssid=\"" + ssid + "\"" + "\n\tpsk=\"" + psk + "\"" +"\n}"
+toWriteToConfigFile = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\r\nupdate_config=1\r\ncountry=CA\r\n\r\nnetwork={\r\n        ssid=\"" + ssid + "\"" + "\r\n        psk=\"" + psk + "\"" +"\r\n        key_mgmt=WPA-PSK\r\n}\r\n\r\n"
 
 f = open('wpa_supplicant.conf', "w")
 f.write(toWriteToConfigFile)
 f.close()
-
 
 subprocess.call(['./copy.sh'])
