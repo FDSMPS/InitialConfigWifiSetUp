121941f605eed93353243d2956d95de1b4533002
Dorsa Nahid <31228267+dorsMaster@users.noreply.github.com>
2020-02-19 16:43:17 -0700

    Update readFile.py

--- readFile.py
+++ readFile.py
@@ -15,12 +15,6 @@ print(cameraCode)
 print(psk)
 print(ssid)
 
-
-# filePathToWrite = '/Users/dors/Desktop/Fifth year/ECE 492/wifiLogin/public'
-# filepath = os.path.join(filePathToWrite, 'wpa_supplicant.conf')
-# if not os.path.exists(filePathToWrite):
-#     os.makedirs(filePathToWrite)
-
 toWriteToConfigFile = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\r\nupdate_config=1\r\ncountry=CA\r\n\r\nnetwork={\r\n        ssid=\"" + ssid + "\"" + "\r\n        psk=\"" + psk + "\"" +"\r\n        key_mgmt=WPA-PSK\r\n}\r\n\r\n"
 
 f = open('wpa_supplicant.conf', "w")
